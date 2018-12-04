0c7877c85b946e987c078a963059ee146581bbb3
Emilio <43657281+Galinbro@users.noreply.github.com>
2018-12-04 15:34:24 -0600

    Update TestIndex.java

--- src/test/java/TestIndex.java
+++ src/test/java/TestIndex.java
@@ -29,7 +29,7 @@ public class TestIndex {
     @BeforeClass
     public static void setUp() {
         //setear el driver
-        System.setProperty("webdriver.chrome.driver","/usr/share/chromedriver3/chromedriver");
+        System.setProperty("webdriver.chrome.driver","chromedriver");
         //abrir chrome
         driver = new ChromeDriver(); 
         driver.get(URL);
